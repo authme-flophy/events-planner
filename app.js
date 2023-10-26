9ae19c7122374775f273d1c49f14d20b0ab061a2
waikau maina <ericmaina.dev@gmail.com>
2023-10-26 12:44:08 +0300

    setup routes in the entry file

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const authRouter = require("./routes/auth");
+const eventRouter = require("./routes/events");
 
 const app = express();
 
-app.listen(4000);
-
 connectDB();
+app.listen(4000, console.log("server running 😃"));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<p>set up successfully</p>");
 });
+
+app.use("/auth", authRouter);
+app.use("/events", eventRouter);
 
 app.use((req, res) => {
   res.status(404).send("<p> page not found </p>");
