7e8c57aa3c7e45633e698503199a3b4ea30be384
waikau maina <ericmaina.dev@gmail.com>
2023-10-26 19:52:08 +0300

    added tests for the login and register endpoints

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -2,22 +2,78 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 const app = require("../app");
 require("dotenv").config();
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
-beforeEach(async () => {
-  await mongoose.connect(process.env.DB_URI);
+let mongoServer;
+
+beforeAll(async () => {
+  // Start an in-memory MongoDB server
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+
+  // Connect to the in-memory database
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 });
 
-afterEach(async () => {
+afterAll(async () => {
   await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+describe("register at /auth/register", () => {
+  it("Should return a success message", async () => {
+    const res = await request(app).post("/auth/register").send({
+      username: "testuser",
+      email: "testuser@email.com",
+      password: "testuser123",
+    });
+    expect(res.statusCode).toBe(201);
+    expect(res.header["token"]).toBeDefined();
+  });
+
+  it("Should return an error message", async () => {
+    const res = await request(app).post("/auth/register").send({
+      username: "testuser",
+      email: "testuser@email.com",
+      password: "testuser123",
+    });
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error).toBe("An account associated with that email exists");
+    expect(res.header["token"]).toBeUndefined();
+  });
 });
 
 describe("login at /auth/login", () => {
   it("Should return success message", async () => {
     const res = await request(app).post("/auth/login").send({
-      email: "ericmaina.dev@gmail.com",
-      password: "waikau123",
+      email: "testuser@email.com",
+      password: "testuser123",
     });
     expect(res.statusCode).toBe(200);
     expect(res.body.message).toBe("Login successful");
-  }, 30000);
+    expect(res.header["token"]).toBeDefined();
+  });
+
+  it("Should return an error message", async () => {
+    const res = await request(app).post("/auth/login").send({
+      email: "testuuser@gmail.com",
+      password: "testuser123",
+    });
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error).toBe("Email or Password is incorrect");
+    expect(res.header["token"]).toBeUndefined();
+  });
+
+  it("Should return an error message", async () => {
+    const res = await request(app).post("/auth/login").send({
+      email: "testuser@gmail.com",
+      password: "teestuser123",
+    });
+    expect(res.statusCode).toBe(400);
+    expect(res.body.error).toBe("Email or Password is incorrect");
+    expect(res.header["token"]).toBeUndefined();
+  });
 });
