7a3cbc53736aceb502e3e440e434cd4d1343f4da
waikau maina <ericmaina.dev@gmail.com>
2023-10-28 16:15:55 +0300

    tested for events creation

--- tests/event.test.js
+++ tests/event.test.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+const eventControllers = require("../controllers/eventControllers");
 const request = require("supertest");
 const app = require("../app");
+const User = require("../models/user");
+const Event = require("../models/event");
 const { MongoMemoryServer } = require("mongodb-memory-server");
 require("dotenv").config();
 
 let mongoServer;
 let token;
+let userId;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
@@ -17,9 +21,22 @@ beforeAll(async () => {
     useUnifiedTopology: true,
   });
 
+  // creating a new user and a token, for authorization and relationship purposes
+  const userData = {
+    username: "testuser",
+    email: "testuser@email.com",
+    password: "testuser123",
+  };
+
+  const user = new User(userData);
+
+  const newUser = await user.save();
+
+  userId = newUser._id.toString();
+
   const payload = {
     user: {
-      id: 1234567,
+      id: newUser._id,
     },
   };
 
@@ -29,6 +46,23 @@ beforeAll(async () => {
 afterAll(async () => {
   await mongoose.connection.close();
   await mongoServer.stop();
+});
+
+describe("Should create a new event", () => {
+  it("Should return the created event", async () => {
+    const res = await request(app)
+      .post("/events")
+      .set("Authorization", token)
+      .send({
+        title: "Test Event Title",
+        description: "Test Event Description",
+        date: "2023-11-01T17:00:00+03:00",
+        location: "Test Event Location",
+        creator: userId,
+      });
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toBeTruthy();
+  });
 });
 
 describe("retrieve all events", () => {
