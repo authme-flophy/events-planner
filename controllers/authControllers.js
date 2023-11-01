59a96f0104c6dbc0062a4f3f04ed0d88bc7a00c2
waikau maina <ericmaina.dev@gmail.com>
2023-11-01 12:43:40 +0300

    created the refresh token controller

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -71,7 +71,40 @@ const register = async (req, res) => {
   }
 };
 
+const refresh_token = async (req, res) => {
+  const refresh_token = req.headers.refresh_token;
+
+  try {
+    if (!refresh_token) {
+      return res.status(400).json({ error: "Unauthorized" });
+    }
+
+    jwt.verify(refresh_token, process.env.REFRESH_SECRET_CODE, (err, user) => {
+      if (err) {
+        return res.status(403).json({ error: "Invalid refresh token" });
+      }
+
+      const user = User.findById(user._id);
+
+      if (!user) {
+        return res.status(403).json({ error: "Invalid refresh token" });
+      }
+
+      const token = user.getToken();
+      refresh_token = user.getRefreshToken();
+
+      res.status(201).set({
+        token: token,
+        refresh_token: refresh_token,
+      });
+    });
+  } catch (err) {
+    res.status(500).json({ error: "server error" });
+  }
+};
+
 module.exports = {
   login,
   register,
+  refresh_token,
 };
