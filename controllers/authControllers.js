9e854b640e8a871debe0e89c4fdf88f3f34a8ad7
waikau maina <ericmaina.dev@gmail.com>
2023-11-01 13:13:33 +0300

    fixed the database query statement

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -72,33 +72,44 @@ const register = async (req, res) => {
 };
 
 const refresh_token = async (req, res) => {
-  const refresh_token = req.headers.refresh_token;
+  let refresh_token = req.headers.refresh_token;
 
   try {
     if (!refresh_token) {
       return res.status(400).json({ error: "Unauthorized" });
     }
 
-    jwt.verify(refresh_token, process.env.REFRESH_SECRET_CODE, (err, user) => {
-      if (err) {
-        return res.status(403).json({ error: "Invalid refresh token" });
+    jwt.verify(
+      refresh_token,
+      process.env.REFRESH_SECRET_CODE,
+      async (err, user) => {
+        if (err) {
+          return res.status(403).json({ error: "Invalid refresh token" });
+        }
+
+        user = await User.findOne({ _id: user.id });
+
+        if (!user) {
+          return res.status(403).json({ error: "Invalid refresh token" });
+        }
+
+        const token = user.getToken();
+        refresh_token = user.getRefreshToken();
+
+        console.log(token);
+        console.log(refresh_token);
+
+        res
+          .status(201)
+          .set({
+            token: token,
+            refresh_token: refresh_token,
+          })
+          .json({ message: "new token" });
       }
-
-      const user = User.findById(user._id);
-
-      if (!user) {
-        return res.status(403).json({ error: "Invalid refresh token" });
-      }
-
-      const token = user.getToken();
-      refresh_token = user.getRefreshToken();
-
-      res.status(201).set({
-        token: token,
-        refresh_token: refresh_token,
-      });
-    });
+    );
   } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "server error" });
   }
 };
