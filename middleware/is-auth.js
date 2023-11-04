5289eafe9510b381dddbb85381c8c9df861468e0
waikau maina <ericmaina.dev@gmail.com>
2023-11-04 12:55:32 +0300

    modified the authentication middleware to check whether the token has been blacklisted

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const TokenBlacklist = require("../models/tokenBlacklist");
 require("dotenv").config();
 
 module.exports = (req, res, next) => {
@@ -8,13 +9,25 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  try {
-    const decodedToken = jwt.verify(authHeader, process.env.SECRET_CODE);
+  TokenBlacklist.findOne({ token }, (error, entry) => {
+    if (error) {
+      return res
+        .status(500)
+        .json({ message: "Error checking token blacklist" });
+    }
 
-    req.userId = decodedToken.user._id;
+    if (entry) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
 
-    next();
-  } catch (error) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
+    try {
+      const decodedToken = jwt.verify(authHeader, process.env.SECRET_CODE);
+
+      req.userId = decodedToken.user._id;
+
+      next();
+    } catch (error) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+  });
 };
